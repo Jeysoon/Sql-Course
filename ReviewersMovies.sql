534de30d3ba1e0f61ff996097cd26b8803657409
Jeyson Mg <jeysoon@gmail.com>
2018-08-29 14:49:35 -0600

    Added challenge 7

--- ReviewersMovies.sql
+++ ReviewersMovies.sql
@@ -153,3 +153,23 @@ FROM   reviewers
        LEFT JOIN reviews 
               ON reviewers.id = reviews.reviewer_id 
 GROUP  BY reviewers.id; 
+
+-- Challenge 7 - 3 TABLES!
+SELECT 
+ title,
+ rating
+ CONCAT(first_name, ' ',last_name) AS reviewer
+FROM series
+
+-- CHALLENGE 7 - 3 TABLES!
+
+SELECT 
+ title,
+ rating,
+ CONCAT(first_name,' ', last_name) as viewer
+FROM reviewers 
+   INNER JOIN reviews
+     ON reviewers.id = reviews.reviewer_id
+   INNER JOIN series
+     ON series.id = reviews.series_id
+ORDER BY title;
